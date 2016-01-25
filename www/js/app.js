82f51169a74532e4520234cc30c717b0aae0ea7b
bhupeshg2 <bhupesh00gupta@gmail.com>
2016-01-25 17:05:14 +0530

    push ng-pintura init commit [non working]

--- www/js/app.js
+++ www/js/app.js
@@ -3,7 +3,10 @@
 // angular.module is a global place for creating, registering and retrieving Angular modules
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
-var approverApp = angular.module('ApproverApp', ['ionic'])
+angular.module('ApproverApp', [
+    'ionic',
+    'ngPintura'
+])
 
 .run(function ($ionicPlatform) {
     $ionicPlatform.ready(function () {
