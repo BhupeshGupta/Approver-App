2bc07e23bb6aa4df021f2caf6714c2e3026887d5
Ravi singh <rvsingh011@gmail.com>
2016-05-12 11:25:05 +0530

    Indent is going to attach with indent and consignment with consignment .

--- www/components/home/home.js
+++ www/components/home/home.js
@@ -1,114 +1,116 @@
 'use strict';
 
 angular.module('ApproverApp')
-    .controller('HomeController', HomeController);
+  .controller('HomeController', HomeController);
 
 
 function HomeController(InvoiceService, SessionService, $rootScope, $scope, $q, $http, $state, RequestFactory, FileFactory, $timeout) {
-    var vm = this;
+  var vm = this;
 
-    /* vars */
-    vm.selectedRequest = "";
-    vm.requests = [];
+  /* vars */
+  vm.selectedRequest = "";
+  vm.requests = [];
 
-    /* functions */
-    vm.showImage = showImage;
-    vm.selectRequest = selectRequest;
-    vm.changeDocstatus = changeDocstatus;
-    vm.reloadRequestList = reloadRequestList;
-    vm.updateInvoiceView = updateInvoiceView;
+  /* functions */
+  vm.showImage = showImage;
+  vm.selectRequest = selectRequest;
+  vm.changeDocstatus = changeDocstatus;
+  vm.reloadRequestList = reloadRequestList;
+  vm.updateInvoiceView = updateInvoiceView;
 
-    vm.selectedImg = {
-        src: '',
-        position: {
-            x: -20,
-            y: -20
-        },
-        scaling: 2,
-        maxScaling: 5,
-        scaleStep: 0.11,
-        mwScaleStep: 0.09,
-        moveStep: 99,
-        fitOnload: false,
-        progress: 0
-    };
+  vm.selectedImg = {
+    src: '',
+    position: {
+      x: -20,
+      y: -20
+    },
+    scaling: 2,
+    maxScaling: 5,
+    scaleStep: 0.11,
+    mwScaleStep: 0.09,
+    moveStep: 99,
+    fitOnload: false,
+    progress: 0
+  };
 
-    function selectRequest(request) {
-        vm.selectedRequest = request;
-        if (vm.selectedRequest.files && vm.selectedRequest.files.length > 0) {
-            vm.selectedImg.src = FileFactory.getLink(vm.selectedRequest.files[0].id);
-        } else {
-            vm.selectedImg.src = '';
-        }
-
-        $state.go('approve.invoice.edit', {
-            requestId: request.requestId
-        });
+  function selectRequest(request) {
+    vm.selectedRequest = request;
+    if (vm.selectedRequest.files && vm.selectedRequest.files.length > 0) {
+      vm.selectedImg.src = FileFactory.getLink(vm.selectedRequest.files[0].id);
+    } else {
+      vm.selectedImg.src = '';
     }
 
-    // on Document select item change value
-    function updateInvoiceView(view) {
-        $state.go(view == 'queue' ? 'approve.invoice.queue' : 'approve.invoice.report');
-
-    }
-
-    function showImage(index) {
-        vm.selectedImg.src = FileFactory.getLink(vm.selectedRequest.files[index].id);
-    }
-
-    function changeDocstatus(request, docstatus) {
-        return $http.post('http://localhost:1337/currentstat/updatestatus', {
-                qid: request.qid,
-                status: docstatus,
-                sid: SessionService.getToken()
-            })
-            .then(function () {
-                var index = vm.requests.indexOf(request);
-                // If the request index is last index, next index will be second last request (reverse queue)
-                // Since we are going to splice last index, we need to account for array length reduction by one
-                var nextRequestIndex = vm.requests.length - 1 == index ? vm.requests.length - 1 - 1 : index;
-                vm.requests.splice(index, 1);
-                vm.selectRequest(vm.requests[nextRequestIndex]);
-            });
-    }
-
-    vm.getMetedata = function (consignmentNumber, docType) {
-        var meta = {
-            'type': 'Consignment Note', //docType,
-            'id': consignmentNumber
-        }
-        InvoiceService.getMetaData(meta).then(
-            function (data) {
-                $scope.metadata = JSON.parse(data.data.message);
-                console.log(data.data.message);
-                var final_data = [];
-                for (var key in $scope.metadata) {
-                    final_data.push({
-                        "key": key,
-                        "value": $scope.metadata[key]
-                    });
-                }
-                vm.metadata_list = final_data;
-            },
-            function (error) {
-                alert(error);
-            }
-        );
-    };
-
-    $rootScope.$on("reloadRequests", function (event, data) {
-        RequestFactory.getReuqests({
-            status: $scope.selectedDocstatusFilter || 0
-        }).then(function (requests) {
-            vm.requests = requests;
-        });
+    $state.go('approve.invoice.edit', {
+      requestId: request.requestId
     });
+  }
 
-    $rootScope.$emit("reloadRequests");
+  // on Document select item change value
+  function updateInvoiceView(view) {
+    $state.go(view == 'queue' ? 'approve.invoice.queue' : 'approve.invoice.report');
 
-    function reloadRequestList() {
-        $rootScope.$emit("reloadRequests");
+  }
+
+  function showImage(index) {
+    vm.selectedImg.src = FileFactory.getLink(vm.selectedRequest.files[index].id);
+  }
+
+  function changeDocstatus(request, docstatus) {
+  
+    return $http.post('http://localhost:1337/currentstat/updatestatus', {
+        qid: request.qid,
+        status: docstatus,
+        sid: SessionService.getToken(),
+        bill_number: $scope.metadata['Bill Number']
+      })
+      .then(function() {
+        var index = vm.requests.indexOf(request);
+        // If the request index is last index, next index will be second last request (reverse queue)
+        // Since we are going to splice last index, we need to account for array length reduction by one
+        var nextRequestIndex = vm.requests.length - 1 == index ? vm.requests.length - 1 - 1 : index;
+        vm.requests.splice(index, 1);
+        vm.selectRequest(vm.requests[nextRequestIndex]);
+      });
+  }
+
+  vm.getMetedata = function(consignmentNumber, docType) {
+    var meta = {
+      'type': 'Consignment Note', //docType,
+      'id': consignmentNumber
     }
+    InvoiceService.getMetaData(meta).then(
+      function(data) {
+        $scope.metadata = JSON.parse(data.data.message);
+        console.log(data.data.message);
+        var final_data = [];
+        for (var key in $scope.metadata) {
+          final_data.push({
+            "key": key,
+            "value": $scope.metadata[key]
+          });
+        }
+        vm.metadata_list = final_data;
+      },
+      function(error) {
+        alert(error);
+      }
+    );
+  };
+
+  $rootScope.$on("reloadRequests", function(event, data) {
+    RequestFactory.getReuqests({
+      status: $scope.selectedDocstatusFilter || 0
+    }).then(function(requests) {
+      vm.requests = requests;
+    });
+  });
+
+  $rootScope.$emit("reloadRequests");
+
+  function reloadRequestList() {
+    $rootScope.$emit("reloadRequests");
+  }
 
 
 }
