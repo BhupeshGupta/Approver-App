8defde5606ae7197312a4c1a8ec83403e077dc15
rvsingh011 <rvsingh011@gmail.com>
2016-09-20 15:08:08 +0530

    Rejected requested Deleted from queue

--- www/js/services/requests.js
+++ www/js/services/requests.js
@@ -1,43 +1,46 @@
 angular
-    .module('ApproverApp')
-    .factory('QueueFactory', queueFactory);
+  .module('ApproverApp')
+  .factory('QueueFactory', queueFactory);
 
 queueFactory.$inject = ['$http', 'ServerUrl', 'SettingsFactory', 'SessionService'];
 
 function queueFactory($http, ServerUrl, SettingsFactory, SessionService) {
-    return {
-        getRequests: getRequests,
-        updateRequestStatus: updateRequestStatus
-    };
+  return {
+    getRequests: getRequests,
+    updateRequestStatus: updateRequestStatus
+  };
 
-    function getRequests(query) {
-
-        return $http.get(SettingsFactory.getReviewServerBaseUrl() + '/queue?' + $.param({
-                where: JSON.stringify(query),
-                limit: 100,
-                sort: 'createdAt asc'
-            }))
-            .then(
-                function (res) {
-                    angular.forEach(res.data, function (r) {
-                        $http.get(SettingsFactory.getReviewServerBaseUrl() + '/files/?parentid=' + r.qid)
-                            .then(function (response) {
-                                r.files = response.data;
-                            });
-                    });
-                    return res.data;
-                });
-
-    }
-
-    function updateRequestStatus(queueId, status) {
-
-        return $http.post(SettingsFactory.getReviewServerBaseUrl() + '/currentstat/updatestatus', {
-            qid: queueId,
-            status: status,
-            sid: SessionService.getToken()
+  function getRequests(query) {
+    return $http.get(SettingsFactory.getReviewServerBaseUrl() + '/queue?' + $.param({
+        where: JSON.stringify(query),
+        limit: 100,
+        sort: 'createdAt asc'
+      }))
+      .then(
+        function(res) {
+          angular.forEach(res.data, function(r) {
+            $http.get(SettingsFactory.getReviewServerBaseUrl() + '/files/?parentid=' + r.qid)
+              .then(function(response) {
+                r.files = response.data;
+              });
+          });
+          return res.data;
         });
 
-    }
+  }
+
+  function updateRequestStatus(queueId, status) {
+
+    return $http.post(SettingsFactory.getReviewServerBaseUrl() + '/currentstat/updatestatus', {
+      qid: queueId,
+      status: status,
+      sid: SessionService.getToken()
+    });
+
+  }
+
+  function deleteRejectedRequestFromQueue(queueId) {
+    return $http.post(SettingsFactory.getReviewServerBaseUrl() + '/queue/destroy' + queueId);
+  }
 
 }
